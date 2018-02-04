50f059c00249ad9ae120085ba4a6c679c004e320
Mike Müller <mike.m.mueller@hotmail.com>
2018-02-04 11:06:54 +0100

    erste Schritte MeinErstes

---
+++ MeinErstes/MeinErstes/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace MeinErstes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdHallo = new System.Windows.Forms.Button();
+            this.cmdEnde = new System.Windows.Forms.Button();
+            this.lblAnzeige = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmdHallo
+            // 
+            this.cmdHallo.Location = new System.Drawing.Point(12, 72);
+            this.cmdHallo.Name = "cmdHallo";
+            this.cmdHallo.Size = new System.Drawing.Size(75, 23);
+            this.cmdHallo.TabIndex = 0;
+            this.cmdHallo.Text = "Hallo";
+            this.cmdHallo.UseVisualStyleBackColor = true;
+            // 
+            // cmdEnde
+            // 
+            this.cmdEnde.Location = new System.Drawing.Point(132, 72);
+            this.cmdEnde.Name = "cmdEnde";
+            this.cmdEnde.Size = new System.Drawing.Size(75, 23);
+            this.cmdEnde.TabIndex = 1;
+            this.cmdEnde.Text = "Ende";
+            this.cmdEnde.UseVisualStyleBackColor = true;
+            // 
+            // lblAnzeige
+            // 
+            this.lblAnzeige.AutoSize = true;
+            this.lblAnzeige.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAnzeige.Location = new System.Drawing.Point(37, 24);
+            this.lblAnzeige.Name = "lblAnzeige";
+            this.lblAnzeige.Size = new System.Drawing.Size(32, 15);
+            this.lblAnzeige.TabIndex = 2;
+            this.lblAnzeige.Text = "(leer)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 256);
+            this.Controls.Add(this.lblAnzeige);
+            this.Controls.Add(this.cmdEnde);
+            this.Controls.Add(this.cmdHallo);
+            this.Name = "Form1";
+            this.Text = "Mein erstes Programm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdHallo;
+        private System.Windows.Forms.Button cmdEnde;
+        private System.Windows.Forms.Label lblAnzeige;
+    }
+}
+
